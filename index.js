0b808e0a5a79497110f88dce4a9dbaa5410dfbf1
madelinemc <madeline.coons@gmail.com>
2021-01-11 14:46:56 -0600

    commit tree b39fee5ecf5f2bdec18d8b6e308f801c54b790bb

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+//First, set up a DOMContentLoaded event in order to detect when the HTML page has loaded, and the document is ready to be manipulated.
+//Use the event's callback function to target the paragraph element with id="text" and replace the text with "This is really cool!"
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateInnerHtml()
+});
+
+function updateInnerHtml() {
+    document.getElementById("text").innerHTML = "This is really cool!";
+}
